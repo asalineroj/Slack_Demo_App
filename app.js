c5c6903de72555a93ecd5c9fed010a3acd1cdd60
Andres Salinero <asalineroj@gmail.com>
2022-02-04 09:35:38 +0000

    updated to capture  messages via RegEx; Added deleting user message (Moderator Function)

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const { App } = require('@slack/bolt');
+// Require the Node Slack SDK package (github.com/slackapi/node-slack-sdk)
+const { WebClient, LogLevel } = require("@slack/web-api");
 
 // Initializes your app with your bot token and signing secret
 const app = new App({
@@ -11,10 +13,15 @@ const app = new App({
   port: process.env.PORT || 3000
 });
 
+const webclient = new WebClient(process.env.SLACK_USER_TOKEN, {
+  // LogLevel can be imported and used to make debugging simpler
+  logLevel: LogLevel.DEBUG
+});
+
 // Listens to incoming messages that contain "hello"
 app.message('hello', async ({ message, say }) => {
   // say() sends a message to the channel where the event was triggered
-  console.log('received message');
+  console.log('received String message');
   await say({
     blocks: [
       {
@@ -22,6 +29,50 @@ app.message('hello', async ({ message, say }) => {
         "text": {
           "type": "mrkdwn",
           "text": `Hey there <@${message.user}>!`
+        },
+        "accessory": {
+          "type": "button",
+          "text": {
+            "type": "plain_text",
+            "text": "Click Me"
+          },
+          "action_id": "button_click"
+        }
+      }
+    ],
+    text: `Hey there <@${message.user}>!`
+  });
+});
+
+app.message(/^(hi|hello|hey).*/, async ({ message, context, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  console.log('received RegEx message');
+  const greeting = context.matches[0];
+
+  console.log(message);
+
+  try {
+    // Call the chat.delete method using the WebClient
+    const result = await webclient.chat.delete({
+      channel: message.channel,
+      ts: message.ts
+    });
+
+    console.log(result);
+  }
+  catch (error) {
+    console.log(error.data.scopes);
+    console.log(error.acceptedScopes);
+    console.error(error);
+  }
+
+  await say({
+    blocks: [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `${greeting} there <@${message.user}>!`
         },
         "accessory": {
           "type": "button",
